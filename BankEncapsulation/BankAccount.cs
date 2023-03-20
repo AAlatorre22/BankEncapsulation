b3d2fd96c77c8434b8d086f730ff44cd29639f53
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-03-20 09:03:46 -0700

    Added all steps

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount() { }
+
+        private double _balance = 0;
+
+        public BankAccount(double amount)
+        {
+            _balance = amount;
+        }
+        public void Deposit(double amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            _balance -= amount;
+
+        }
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        //public void AtBank()
+        //{
+        //    Console.WriteLine("Hello. Welcome to your bank.\n" +
+        //        "What would you like to do?" +
+        //        "");
+        //} 
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace BankEncapsulation
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var alejandrosAcct = new BankAccount();
+
+
+            Console.WriteLine("Hello. Welcome to your bank.\n" +
+                "What would you like to do?" +
+                " Please type: \n1 for deposit\n2 for withdraw\n" +
+                "3 to check balance");
+            var x = int.Parse(Console.ReadLine());
+            if (x == 1)
+            {
+                Console.WriteLine("How much would you like to deposit?");
+                var deposit = int.Parse(Console.ReadLine());
+                alejandrosAcct.Deposit(deposit);
+                Console.WriteLine($"Your new amount is now ${alejandrosAcct.GetBalance()}");
+              
+
+            }
+            if(x == 2)
+            {
+                Console.WriteLine("How much would you like to withdraw?");
+                var withdraw = int.Parse(Console.ReadLine());
+                alejandrosAcct.Withdraw(withdraw);
+                Console.WriteLine($"Your new amount is now ${alejandrosAcct.GetBalance()}");
+                if (alejandrosAcct.GetBalance()  < 0)
+                {
+                    Console.WriteLine("Uh oh! Looks like you have overdrawn your account!\nWe are notifying the IRS and calling your mother!");
+                }
+            }
+            if (x == 3)
+            {
+
+            Console.WriteLine($"Your balance is ${alejandrosAcct.GetBalance()} Don't spend it all in one place!");
+            }
+
+
+
         }
     }
 }
